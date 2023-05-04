44d73f5bbbf8e04192e3383e159b57da02dcd96a
Milan Lenco <milan@zededa.com>
2023-05-04 18:09:12 +0200

    Maybe fix dev_local_info.
    Signed-off-by: Milan Lenco <milan@zededa.com>

--- tests/reboot/reboot_test.go
+++ tests/reboot/reboot_test.go
@@ -51,7 +51,7 @@ func checkReboot(t *testing.T, edgeNode *device.Ctx) projects.ProcInfoFunc {
 		currentLastRebootTime := im.GetDinfo().LastRebootTime
 		if !proto.Equal(lastRebootTime, currentLastRebootTime) {
 			if im.GetDinfo().LastRebootReason == "" &&
-				lastRebootTime.AsTime().IsZero() {
+				currentLastRebootTime.AsTime().IsZero() {
 				// device may not fill the info
 				return nil
 			}
